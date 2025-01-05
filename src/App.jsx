c9e85dc1132a642433c746431d11e9a45a5cb0da
jungdonguk <foodsmith96@naver.com>
2025-01-05 21:27:19 +0900

    폰트/이미지 적용 후후 공통 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Header from "./components/Header";
+import Button from "./components/Button";
 import NotFoud from "./pages/NotFoud";
+import { getEmotionImages } from "./util/get-emotion-iamges";
 
 function App() {
   const navigation = useNavigate();
@@ -13,6 +16,16 @@ function App() {
 
   return (
     <>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+
+      <Button text="버튼" type="DEFAULT" />
+      <Button text="버튼" type="POSITIVE" />
+      <Button text="버튼" type="NEGATIVE" />
+
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
